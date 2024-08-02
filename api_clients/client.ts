1563bc07510fa5e6458f9028862d3e23f357cacf
syoumakuwaba <syoumakuwaba@icloud.com>
2024-08-02 06:00:12 +0000

    client generated

--- api_clients/client.ts
+++ api_clients/client.ts
@@ -1,7 +1,53 @@
 import { makeApi, Zodios, type ZodiosOptions } from "@zodios/core";
 import { z } from "zod";
 
+const PostCreatorBody = z
+  .object({
+    user_id: z.number().int(),
+    contact_address: z.string(),
+    transfer_target: z.string(),
+  })
+  .passthrough();
+const ValidationError = z
+  .object({
+    loc: z.array(z.union([z.string(), z.number()])),
+    msg: z.string(),
+    type: z.string(),
+  })
+  .passthrough();
+const HTTPValidationError = z
+  .object({ detail: z.array(ValidationError) })
+  .partial()
+  .passthrough();
+
+export const schemas = {
+  PostCreatorBody,
+  ValidationError,
+  HTTPValidationError,
+};
+
 const endpoints = makeApi([
+  {
+    method: "post",
+    path: "/creator",
+    alias: "post_creator_creator_post",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: PostCreatorBody,
+      },
+    ],
+    response: z.unknown(),
+    errors: [
+      {
+        status: 422,
+        description: `Validation Error`,
+        schema: HTTPValidationError,
+      },
+    ],
+  },
   {
     method: "get",
     path: "/health",
