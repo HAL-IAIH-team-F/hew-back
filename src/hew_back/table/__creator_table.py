8cccb69b98c90e63e88483b301f448ab56fbd88d
atsuya <f.atsuya.personal@gmail.com>
2024-10-06 22:25:21 +0900

    push依頼

--- src/hew_back/table/__creator_table.py
+++ src/hew_back/table/__creator_table.py
@@ -12,8 +12,7 @@ class CreatorTable(BaseTable):
     creator_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id: Mapped[uuid.UUID]  = Column(UUID(as_uuid=True), ForeignKey('TBL_USER.user_id'), nullable=False)
     contact_address = Column(String(64), nullable=False)
-    transfer_target = Column(String(64), nullable=False)
-    # 振込先
+    transfer_target = Column(String(64), nullable=False) # 振込先
 
 
 
