080bab835ee41d4894fa4ab7059d23bbad498064
atsuya <f.atsuya.personal@gmail.com>
2024-11-29 14:22:33 +0900

    real_finish

--- src/migrations/versions/64ff91ffb65c_.py
+++ src/migrations/versions/64ff91ffb65c_.py
@@ -1,7 +1,7 @@
 """empty message
 
 Revision ID: 64ff91ffb65c
-Revises: cac1fe43e0ae, fec742e30493
+Revises:  fec742e30493
 Create Date: 2024-11-29 03:09:24.106715
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '64ff91ffb65c'
-down_revision: Union[str, None] = ('cac1fe43e0ae', 'fec742e30493')
+down_revision: Union[str, None] = 'fec742e30493'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
