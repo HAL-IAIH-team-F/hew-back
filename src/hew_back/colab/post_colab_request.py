79e631c326359c641066bbc07eb1ef4a2d66aa2e
kigawa <kigawa.8390@gmail.com>
2024-12-07 21:26:11 +0900

    add: post colab request endpoint

--- src/hew_back/colab/post_colab_request.py
+++ src/hew_back/colab/post_colab_request.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Union, Sequence
 
 import pydantic.dataclasses
@@ -5,8 +6,7 @@ import sqlalchemy.ext.asyncio
 from fastapi import Depends, Query
 from sqlalchemy import ColumnElement
 
-from hew_back import deps, app
-from hew_back.recruit.__reses import RecruitRes
+from hew_back import deps, app, tbls
 from hew_back.tbls import RecruitTable
 
 
@@ -37,26 +37,55 @@ async def find_recruits(
     for recruit in result:
         await session.refresh(recruit)
     return result
+
+
 @pydantic.dataclasses.dataclass
-class PostColabRequest:
+class PostColabRequestBody:
+    recruit_id: uuid.UUID
+
+
+@pydantic.dataclasses.dataclass
+class __Service:
     def __init__(
             self,
-        session: sqlalchemy.ext.asyncio.AsyncSession = Depends(deps.DbDeps.session),
+            body: PostColabRequestBody,
+            session: sqlalchemy.ext.asyncio.AsyncSession = Depends(deps.DbDeps.session),
+            sender: deps.CreatorDeps = Depends(deps.CreatorDeps.get),
     ):
         self.session = session
+        self.sender = sender
+        self.body = body
 
-    def send_request(self):
-        tbls
+    async def insert_notification(self) -> tbls.NotificationTable:
+        notification = tbls.NotificationTable()
+        self.session.add(notification)
+        await self.session.refresh(notification)
+        return notification
+
+    async def select_recruit(self) -> tbls.RecruitTable:
+        recruit = await self.session.execute(
+            sqlalchemy.select(tbls.RecruitTable)
+            .where(tbls.RecruitTable.recruit_id == self.body.recruit_id)
+        )
+        return recruit.scalar_one()
+
+    async def insert_colab_notification(self) -> tbls.NotificationCollaboTable:
+        notification = await self.insert_notification()
+        recruit = await self.select_recruit()
+        colab = tbls.NotificationCollaboTable(
+            notification_id=notification.notification_id,
+            sender_creator_id=self.sender.creator_table.creator_id,
+            receive_creator_id=recruit.creator_id,
+        )
+        return colab
+
+    async def send_request(self):
+        await self.insert_colab_notification()
+
 
 @app.post("/api/colab/request")
 async def pcr(
-        recruits: PostColabRequest = Depends(PostColabRequest),
-) -> list[RecruitRes]:
-    result: list[RecruitRes] = []
-
-    for recruit in recruits:
-        result.append(RecruitRes(
-            recruit.recruit_id, recruit.creator_id, recruit.title, recruit.description
-        ))
-
-    return result
+        service: __Service = Depends(__Service),
+) -> None:
+    await service.send_request()
+    return
