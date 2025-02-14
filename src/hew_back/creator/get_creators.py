a3a96cc31abe7b77ec622ee1455775f2084a90d3
kigawa <kigawa.8390@gmail.com>
2025-02-14 11:06:00 +0900

    fix: type

--- src/hew_back/creator/get_creators.py
+++ src/hew_back/creator/get_creators.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from hew_back import deps, app, tbls
 from hew_back.creator.__creator_service import CreatorService
 from hew_back.creator.__res import CreatorResponse
+from hew_back.user.__user_service import UserService
 
 
 class __Service:
@@ -11,9 +12,11 @@ class __Service:
             self,
             session: sqlalchemy.ext.asyncio.AsyncSession = Depends(deps.DbDeps.session),
             creator_service: CreatorService = Depends(),
+            user_service: UserService = Depends(),
     ):
         self.session = session
         self.__creator_service = creator_service
+        self.__user_service = user_service
 
     async def select_creators(self) -> list[tbls.CreatorTable]:
         records = await self.session.execute(
@@ -27,7 +30,7 @@ class __Service:
         return [CreatorResponse(
             creator_id=creator.creator_id,
             contact_address=creator.contact_address,
-            user_data=self.__creator_service.create_user_data(await self.__creator_service.select_user(creator))
+            user_data=self.__creator_service.create_user_data(await self.__user_service.select_user(creator))
         ) for creator in creators]
 
 
