7a9797b17fa7fe29b9d3fcc0f8d81ee0009e53bd
kigawa <kigawa.8390@gmail.com>
2025-02-14 14:22:43 +0900

    fix: type

--- src/hew_back/user/put_user.py
+++ src/hew_back/user/put_user.py
@@ -3,6 +3,7 @@ import sqlalchemy
 from fastapi import Depends
 
 from hew_back import app, deps, mdls
+from hew_back.creator.__creator_service import CreatorService
 from hew_back.user.__body import UserBody
 from hew_back.user.__res import SelfUserRes
 from hew_back.user.__user_service import UserService
@@ -16,33 +17,21 @@ class __Service:
             session: sqlalchemy.ext.asyncio.AsyncSession = Depends(deps.DbDeps.session),
             user: deps.UserDeps = Depends(deps.UserDeps.get),
             user_service: UserService = Depends(),
+            creator_service: CreatorService = Depends(),
     ):
         self.__session = session
         self.__user = user
         self.__response = response
         self.__user_service = user_service
         self.__body = body
+        self.__creator_service = creator_service
 
     async def process(self) -> SelfUserRes:
         self.__user.user_table.user_icon_uuid = self.__body.user_icon_uuid
         self.__user.user_table.user_name = self.__body.user_name
-        if self.__body.user_icon_uuid is not None:
-            await self.__user_service.post_images(self.__body.user_icon_uuid)
-            user_icon = mdls.File(
-                image_uuid=self.__user.user_table.user_icon_uuid,
-                token=None,
-            )
-        else:
-            user_icon = None
         await self.__session.flush()
-        return SelfUserRes(
-            user_id=self.__user.user_table.user_id,
-            user_name=self.__user.user_table.user_name,
-            user_screen_id=self.__user.user_table.user_screen_id,
-            user_icon=user_icon,
-            user_date=self.__user.user_table.user_date,
-            user_mail=self.__user.user_table.user_mail,
-        )
+        creator = await self.__creator_service.select_creator_or_none(self.__user.user_table.user_id)
+        return UserService.create_user_res(self.__user.user_table, CreatorService.create_creator_data(creator))
 
 
 @app.put("/api/user")
